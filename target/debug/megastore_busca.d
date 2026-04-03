99d70654279dcd635524cbe6c780ff07f8bf7bc1
Charles Kendy Tanaka <charleskendy672@gmail.com>
2026-04-03 16:20:10 -0300

    removendo target

--- target/debug/megastore_busca.d
+++
@@ -1,1 +0,0 @@
-C:\Users\charl\megastore_busca\target\debug\megastore_busca.exe: C:\Users\charl\megastore_busca\src\busca.rs C:\Users\charl\megastore_busca\src\main.rs C:\Users\charl\megastore_busca\src\produto.rs
