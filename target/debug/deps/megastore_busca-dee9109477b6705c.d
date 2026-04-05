c774d3eda5e40c2731f0f34c859339e083849afa
Charles Kendy Tanaka <charleskendy672@gmail.com>
2026-04-05 12:27:54 -0300

    corrigindo estrutura de testes

---
+++ target/debug/deps/megastore_busca-dee9109477b6705c.d
@@ -0,0 +1,7 @@
+C:\Users\charl\megastore_busca\target\debug\deps\megastore_busca-dee9109477b6705c.d: src\main.rs src\produto.rs src\busca.rs
+
+C:\Users\charl\megastore_busca\target\debug\deps\megastore_busca-dee9109477b6705c.exe: src\main.rs src\produto.rs src\busca.rs
+
+src\main.rs:
+src\produto.rs:
+src\busca.rs:
